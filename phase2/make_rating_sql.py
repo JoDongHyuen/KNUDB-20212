ab01898e86f97121bead3fb52fdd5ca8d05f7419
dlwnsqo <75034782+dlwnsqo@users.noreply.github.com>
2021-10-12 16:50:16 +0900

    rating

--- phase2/make_rating_sql.py
+++ phase2/make_rating_sql.py
@@ -3,24 +3,31 @@ import random
 import os
 
 f = open('C:/Users/Boat/Downloads/rating.csv  ', 'r')
+f2 = open('C:/Users/Boat/Downloads/simple_merged_ID.csv  ', 'r')
 ff = open('C:/Users/Boat/Desktop/ㅈㅂ/데이터베이스/팀프/rating.sql', 'w')
 
 rdr = csv.reader(f)
+rdr2 = csv.reader(f2)
 id = []
-count = 0
+email = []
+count = 1
 
 for line in rdr:
     id.append(line[0])
 
-for i in range(1194):
-    #sql = "insert into beverage (B_BRNO, DRINKID, ALCOHOL, DRINKNAME) values (:1, :2, :3, :4)"
+for line in rdr2:
+    email.append(line[2])
 
-    score = random.randrange(2, 11) * 0.5
-    if(i%3 == 0):
+for i in range(len(email)-1):
+
+    for j in range(random.randrange(0, 3)):
+        bnum = random.randrange(1, 399)
+        score = random.randrange(2, 11) * 0.5
+
+        ff.write("insert into rating values (" + str(bnum))
+        ff.write(", " + email[i+1])
+        ff.write(", " + str(score))
+        ff.write(", " + str(count))
+        ff.write(");\n")
+
         count += 1
-
-    ff.write("insert into rating values (" + str(count))
-    ff.write(", 'email'")
-    ff.write(", " + str(score))
-    ff.write(", " + id[i])
-    ff.write(");\n")
